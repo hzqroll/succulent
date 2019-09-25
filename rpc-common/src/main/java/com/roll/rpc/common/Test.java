0305e97fa1515ba79a9946aebc9b13b820dac253
hzqroll <haozongqiang@foxmail.com>
2019-09-25 08:31:29 +0800

    clean

--- rpc-common/src/main/java/com/roll/rpc/common/Test.java
+++
@@ -1,8 +0,0 @@
-package com.roll.rpc.common;
-
-/**
- * @author roll
- * created on 2019-09-03 17:07
- */
-public class Test {
-}
--- succulent-flower/src/main/java/com/roll/succulentflower/queenslave/websocket/handler.java
+++
@@ -1,8 +0,0 @@
-package com.roll.succulentflower.queenslave.websocket;
-
-/**
- * @author zongqiang.hao
- * created on 2019-06-02 12:07.
- */
-public class handler {
-}
