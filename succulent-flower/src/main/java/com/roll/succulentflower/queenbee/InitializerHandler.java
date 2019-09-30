74dd595b94f59f1799df143e25d8db145f047571
hzqroll <haozongqiang@foxmail.com>
2019-09-30 09:29:28 +0800

    init

--- succulent-flower/src/main/java/com/roll/succulentflower/queenbee/InitializerHandler.java
+++ succulent-flower/src/main/java/com/roll/succulentflower/queenbee/InitializerHandler.java
@@ -2,7 +2,7 @@ package com.roll.succulentflower.queenbee;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
-import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
@@ -10,10 +10,10 @@ import io.netty.handler.codec.string.StringEncoder;
  * @author roll
  * created on 2019-09-25 08:59
  */
-public class InitializerHandler extends ChannelInitializer<NioSocketChannel> {
+public class InitializerHandler extends ChannelInitializer<SocketChannel> {
     @Override
-    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
-        ChannelPipeline pipeline = nioSocketChannel.pipeline();
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
         pipeline.addLast(new StringDecoder());
         pipeline.addLast(new StringEncoder());
         pipeline.addLast(new MessageHandler());
